703061375ed1d3a37835bb5f18dd2115af3b79f5
username <wecp-dummy@gmail.com>
2024-02-28 05:48:29 +0000

    ViewEventsComponent done & httpservice getEvents & addEvent

--- client/src/app/view-events/view-events.component.ts
+++ client/src/app/view-events/view-events.component.ts
@@ -2,7 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { HttpService } from '../../services/http.service';
-import { AuthService } from '../../services/auth.service';
+import { FormsModule } from '@angular/forms';
+import { Observable, of } from 'rxjs';
+// import { HttpService } from '../../services/http.service';
+// import { AuthService } from '../../services/auth.service';
 
 
 @Component({
@@ -12,45 +15,73 @@ import { AuthService } from '../../services/auth.service';
 })
 export class ViewEventsComponent implements OnInit {
 
-  itemForm: FormGroup; 
-  formModel:any={status:null};
-  showError:boolean=false;
-  errorMessage:any;
-  eventObj:any=[];
-  assignModel: any={};
+  itemForm: FormGroup;
+  formModel: any = { status: null };
+  showError: boolean = false;
+  errorMessage: any;
+  eventObj$: Observable<any> = of([]);
+  assignModel: any = {};
 
   showMessage: any;
   responseMessage: any;
-  isUpdate: any=false;;
-  constructor(public router:Router, public httpService:HttpService, private formBuilder: FormBuilder, private authService:AuthService) 
-    {
-      // this.itemForm =complete this form init
-   
+  isUpdate: any = true;
+
+  inputMessage: string = '';
+  userList$: Observable<any> = of([]);
+  errorMsg$: Observable<String> = of('');
+  responseMsg$: Observable<String> = of('');
+  event:any;
+  constructor(public router: Router, private formBuilder: FormBuilder, private httpService: HttpService) {
+    // this.itemForm =complete this form init
+    this.itemForm = formBuilder.group({
+      eventID: [''],
+      title: ['', [Validators.required]],
+      description: ['', [Validators.required]],
+      dateTime: ['', [Validators.required]],
+      location: ['', [Validators.required]],
+      status: ['', [Validators.required]],
+      user: ['', [Validators.required]]
+    });
   }
   ngOnInit(): void {
 
-  
+
   }
   searchEvent() {
-  //complete this function
-    
-  
-  }
-
- 
-  onSubmit()
-  {
     //complete this function
+    this.eventObj$ = this.httpService.getEventById(this.inputMessage);
   }
-  edit(val:any)
-  {
-    this.isUpdate=true;
-    let dateTime=new Date(val.dateTime);
-    this.itemForm.patchValue({
+
+
+  onSubmit() {
     //complete this function
-
-    })
+    if (this.itemForm.valid) {
+      console.log(this.inputMessage);
+      this.httpService.updateEvent(this.itemForm.value, this.inputMessage).subscribe(
+        (res: any) => {
+          this.responseMsg$ = of('Event updated successfully');
+          this.searchEvent();
+        },
+        (error: any) => {
+          this.errorMsg$ = of('Unable to update event');
+        }
+      )
+      this.isUpdate = true;
+    }else{
+      this.itemForm.markAllAsTouched();
+    }
   }
+  edit(val: any) {
+    this.isUpdate = false;
+    let dateTime = new Date(val.dateTime);
+    // this.itemForm.patchValue({
+    // //complete this function
+    // })
 
-  
+    this.eventObj$.subscribe((data: any) => {
+      this.event = data[0];
+      this.itemForm.patchValue(data[0]);
+      console.log(data);
+    });
+  }
 }
--- client/src/services/http.service.ts
+++ client/src/services/http.service.ts
@@ -3,63 +3,64 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { map, Observable, retry } from 'rxjs';
 import { environment } from '../environments/environment.development';
 import { AuthService } from './auth.service';
- 
+
 @Injectable({
   providedIn: 'root'
 })
 export class HttpService {
-  public serverName=environment.apiUrl;
-  constructor(private http : HttpClient) {
- 
-   }
- 
-  getEventById(eventId:any):Observable<any> {
-    //complete this function
-  
+  public serverName = environment.apiUrl;
+  constructor(private http: HttpClient) {
+
   }
-  updateEvent(newEvent:any,eventId:any):Observable<any> {
-   //complete this function
+
+  getEventById(eventId: any): Observable<any> {
+    //complete this function
+
+  }
+  updateEvent(newEvent: any, eventId: any): Observable<any> {
+    //complete this function
 
   }
   // assignDriver(driverid: any, cargoId: any): Observable<any> {
   //  //complete this function
   // }
- 
+
   // getAssignOrders(driverId:any):Observable<any> {
   // //complete this function
   // }
-  getResources():Observable<any> {
-   //complete this function
-  
-  }
-  addAllocateResource(data:any):Observable<any>{
-   
-  }
- 
-  addResource(data : any): Observable<any> {
+  getResources(): Observable<any> {
+    //complete this function
 
   }
- 
-  getEvents():Observable<any> {
-  //complete this function
+  addAllocateResource(data: any): Observable<any> {
 
   }
-  addEvent(details:any):Observable<any> {
-  //complete this function
-  console.log(details);
+
+  addResource(data: any): Observable<any> {
 
   }
-  Login(details:any):Observable<any> {
-  //complete this function
- 
+
+  getEvents(): Observable<any> {
+    //complete this function
+    return this.http.get(`${this.serverName}/api/planner/events`);
   }
-  registerUser(details:any):Observable<any> {
-   //complete this function
-   console.log(details);
-   
-  
+  addEvent(details: any): Observable<any> {
+    //complete this function
+    console.log(details);
+
+    return this.http.post(`${this.serverName}/api/planner/event`, details);
   }
-  getAllUser():Observable<any>{
-   
+  Login(details: any): Observable<any> {
+    //complete this function
+
+  }
+  registerUser(details: any): Observable<any> {
+    //complete this function
+    console.log(details);
+
+
+  }
+  getAllUser(): Observable<any> {
+
   }
 }
