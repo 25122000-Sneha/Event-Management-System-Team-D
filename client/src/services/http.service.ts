13c8951c505084cb7acc82a4683168851add0b11
username <wecp-dummy@gmail.com>
2024-02-28 05:56:43 +0000

    added create-event component & login-cmponent, two methods in httpservice

--- client/src/services/http.service.ts
+++ client/src/services/http.service.ts
@@ -13,13 +13,21 @@ export class HttpService {
  
    }
  
-  getEventById(eventId:any):Observable<any> {
+   getEventById(eventId:any):Observable<any> {
     //complete this function
-  
+    return this.http.get(`${this.serverName}/api/staff/event-details/`+eventId).pipe(
+      map((data: any)=>{
+        if(Array.isArray(data)){
+          return data;
+        }else{
+          return [data];
+        }
+      })
+    );
   }
   updateEvent(newEvent:any,eventId:any):Observable<any> {
    //complete this function
-
+   return this.http.put<Event>(`${this.serverName}/api/staff/update-setup/${eventId}`,newEvent);
   }
   // assignDriver(driverid: any, cargoId: any): Observable<any> {
   //  //complete this function
